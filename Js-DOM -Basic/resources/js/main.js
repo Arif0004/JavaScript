9fb99dda5713fc531b8eba29f3849328fab77adc
Arif0004 <arifmahmudaj6@gmail.com>
2021-07-01 10:23:01 +0600

    Js

---
+++ Js-DOM -Basic/resources/js/main.js
@@ -0,0 +1,56 @@
+const $ = (data) => document.querySelector(data)
+const print = (data) => console.log(data)
+const create = (data) =>document.createElement(data)
+
+// //create
+// let a = document.createElement('a')
+// a.innerText = "Click me1"
+// a.className= 'btn btn-sm btn-success'
+// a.setAttribute('id' , 'btn1')
+
+// let a2 = document.createElement('a')
+// a2.innerText = "Click me2"
+// a2.className= 'btn btn-sm btn-warning mx-2'
+// a2.setAttribute('id' , 'btn2')
+
+// let a3 = document.createElement('a')
+// a3.innerText = "Click me3"
+// a3.className= 'btn btn-sm btn-info mx-2'
+// a3.setAttribute('id' , 'btn3')
+
+// let a4 = document.createElement('a')
+// a4.innerText = "Click me4"
+// a4.className= 'btn btn-sm btn-danger mx-2'
+// a4.setAttribute('id' , 'btn4')
+
+
+// print(a)
+
+// //append
+
+// let app = $('#app')
+
+// app.appendChild(a)
+// app.appendChild(a2)
+// app.appendChild(a3)
+// app.appendChild(a4)
+
+// let btn2 = $('#btn2')
+
+// //remove
+// //app.removeChild(btn2)
+
+// //replace
+// app.replaceChild(a4,btn2)
+
+//event listener
+
+// let btn = $('#btn')
+
+// btn.addEventListener('click', function(){
+
+//     print("I am clicked")
+
+// })
+ 
+ 
